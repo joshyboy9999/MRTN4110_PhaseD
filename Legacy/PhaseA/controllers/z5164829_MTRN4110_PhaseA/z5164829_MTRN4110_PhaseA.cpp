f085700b73ca20588e5db891aee4bfadfd1a5a48
Tobin Smit <tobinksmit@gmail.com>
2020-08-11 14:57:37 +1000

    Added Phase A and B to legacy

---
+++ Legacy/PhaseA/controllers/z5164829_MTRN4110_PhaseA/z5164829_MTRN4110_PhaseA.cpp
@@ -0,0 +1,78 @@
+// File:          z5164829_controller.cpp
+// Date:
+// Description:
+// Author:
+// Modifications:
+
+// You may need to add webots include files such as
+// <webots/DistanceSensor.hpp>, <webots/Motor.hpp>, etc.
+// and/or to add some other includes
+#include <webots/Robot.hpp>
+#include <webots/Motor.hpp>
+#include <webots/DistanceSensor.hpp>
+#include <webots/PositionSensor.hpp>
+#include <webots/Compass.hpp>
+#include <fstream>
+#include <filesystem>
+
+#include "Micromouse.hpp"
+
+// All the webots classes are defined in the "webots" namespace
+using namespace webots;
+
+#define PATH_PLAN_FILE_NAME "../../PathPlan.txt"
+
+std::string getPathPlanString(std::string path) { 
+  std::ifstream ifs (path);
+  std::string str;
+  std::getline(ifs, str);
+  return str;
+}
+
+int main(int argc, char **argv) {
+  Micromouse* micromouse = new Micromouse();
+
+  std::cout << "Start - Read path plan from " << PATH_PLAN_FILE_NAME << ":\n";
+  std::string pathPlanString = getPathPlanString(PATH_PLAN_FILE_NAME);
+  std::cout << pathPlanString << "\n";
+  std::cout << "Done - Path plan read!\n";
+
+  int step = 0;
+  int startRow = pathPlanString.at(0) - '0';
+  int startCol = pathPlanString.at(1) - '0';
+  char headingLetter = pathPlanString.at(2);
+  int startHeading = 0;
+  switch (headingLetter) {
+    case 'E':
+      startHeading = 0; break;
+    case 'N':
+      startHeading = 1; break;
+    case 'W':
+      startHeading = 2; break;
+    case 'S':
+      startHeading = 3; break;
+  }
+  micromouse->setRowColHeading(startRow,startCol,startHeading);
+
+  std::cout << "Start - Execute path plan!\n";
+  micromouse->step(micromouse->timeStep);
+  for (int i = 3; i < pathPlanString.length(); i++) {
+    micromouse->printState(step);
+    switch (pathPlanString.at(i)) {
+      case 'L':
+        micromouse->turnLeft(); break;
+      case 'F':
+        micromouse->goStraight(); break;
+      case 'R':
+        micromouse->turnRight(); break;
+    }
+    // finishTask(robot, timeStep, leftEncoder, rightEncoder, frontProx);
+    step++;
+  }
+  micromouse->printState(step);
+  std::cout << "Done - Path plan executed!\n";
+  delete micromouse;
+  return 0;
+  
+}
+

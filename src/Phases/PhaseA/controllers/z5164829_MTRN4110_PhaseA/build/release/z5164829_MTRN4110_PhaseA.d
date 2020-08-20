e50122a15098b6170f8040b9d1b8d3308ce6d690
joshyboy9999 <z5161697@ad.unsw.edu.au>
2020-08-20 12:53:27 +1000

    Folder Restructure, and working Main.py with usr input

--- src/Phases/PhaseA/controllers/z5164829_MTRN4110_PhaseA/build/release/z5164829_MTRN4110_PhaseA.d
+++ src/Phases/PhaseA/controllers/z5164829_MTRN4110_PhaseA/build/release/z5164829_MTRN4110_PhaseA.d
@@ -1,9 +1,9 @@
 build/release/z5164829_MTRN4110_PhaseA.o: z5164829_MTRN4110_PhaseA.cpp \
-  /Applications/Webots.app/include/controller/cpp/webots/Robot.hpp \
-  /Applications/Webots.app/include/controller/cpp/webots/Motor.hpp \
-  /Applications/Webots.app/include/controller/cpp/webots/Device.hpp \
-  /Applications/Webots.app/include/controller/cpp/webots/../../c/webots/types.h \
-  /Applications/Webots.app/include/controller/cpp/webots/DistanceSensor.hpp \
-  /Applications/Webots.app/include/controller/cpp/webots/PositionSensor.hpp \
-  /Applications/Webots.app/include/controller/cpp/webots/Compass.hpp \
-  Micromouse.hpp
+ C:\Users\joshy\AppData\Local\Programs\Webots/include/controller/cpp/webots/Robot.hpp \
+ C:\Users\joshy\AppData\Local\Programs\Webots/include/controller/cpp/webots/Motor.hpp \
+ C:\Users\joshy\AppData\Local\Programs\Webots/include/controller/cpp/webots/Device.hpp \
+ C:/Users/joshy/AppData/Local/Programs/Webots/include/controller/c/webots/types.h \
+ C:\Users\joshy\AppData\Local\Programs\Webots/include/controller/cpp/webots/DistanceSensor.hpp \
+ C:\Users\joshy\AppData\Local\Programs\Webots/include/controller/cpp/webots/PositionSensor.hpp \
+ C:\Users\joshy\AppData\Local\Programs\Webots/include/controller/cpp/webots/Compass.hpp \
+ Micromouse.hpp
